5bd7a7a2cde8e5ccd4661549271d3a3a4faf628a
Jonathan Gruber <gruberjonathan@gmail.com>
2023-12-15 22:27:40 +0100

    I'm a mac person now 🫣

--- create-release.sh
+++ create-release.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env sh
+set -e
+
+mkdir -p dist
 rm -f dist/*
-tar --exclude='dist' --exclude='.git' --exclude='.idea' \
+gtar --exclude='dist' --exclude='.git' --exclude='.idea' \
   -czf dist/googledocsredirect.tar.gz --transform 's,^,googledocsredirect/,' *
 
 echo "Signature:"
 openssl dgst -sha512 -sign ~/.nextcloud/certificates/googledocsredirect.key dist/googledocsredirect.tar.gz | openssl base64
 
-echo -e "\nNow visit:\nhttps://apps.nextcloud.com/developer/apps/releases/new"
+echo  "\nNow visit: https://apps.nextcloud.com/developer/apps/releases/new"
