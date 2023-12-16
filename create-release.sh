32d9fdd7fae845537ce953f9a4bb17d8ce74c366
Jonathan Gruber <gruberjonathan@gmail.com>
2023-12-16 21:32:38 +0100

    Make script OS independent
    Only reasonable systems of course

--- create-release.sh
+++ create-release.sh
@@ -1,12 +1,21 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 set -e
+shopt -s expand_aliases
+
+# Use gnu-tar in mac OS
+if [[ "$OSTYPE" == darwin* ]]; then
+  alias tar='gtar'
+else
+  echo "nope"
+fi
 
 mkdir -p dist
 rm -f dist/*
-gtar --exclude='dist' --exclude='.git' --exclude='.idea' \
+tar --exclude='dist' --exclude='.git' --exclude='.idea' \
   -czf dist/googledocsredirect.tar.gz --transform 's,^,googledocsredirect/,' *
 
 echo "Signature:"
 openssl dgst -sha512 -sign ~/.nextcloud/certificates/googledocsredirect.key dist/googledocsredirect.tar.gz | openssl base64
 
-echo  "\nNow visit: https://apps.nextcloud.com/developer/apps/releases/new"
+echo "\nTag the commit and publish a release on GitHub"
+echo "Then visit: https://apps.nextcloud.com/developer/apps/releases/new"
